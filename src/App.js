7495c49f5d593685d964bff34b2a6b4fe01fa9eb
Steel <https://github.com/Steelmen87/>
2022-03-13 05:32:21 +0300

    start step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
+import Header from "./components/Header";
+import Sidebar from "./components/SideBar";
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="page">
+            <div className="header">
+                <Header/>
+            </div>
+            <aside className="sidebar">
+                <Sidebar/>
+            </aside>
+            <main className="main">
+                <div className="container">
+                    <Main/>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
