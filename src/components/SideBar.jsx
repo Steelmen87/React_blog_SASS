c0b73fc1c126f1f48d3ae42471a3fc43f1cc7109
Steel <https://github.com/Steelmen87/>
2022-03-13 07:00:54 +0300

    next footer

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import sidebarHeader from "../assets/images/sidebar-header.jpg"
+import insta from "../assets/images/instagram.svg"
+import vk from "../assets/images/vk.svg"
+import pinterest from "../assets/images/pinterest.svg"
 
-export default function Sidebar(){
+export default function Sidebar() {
+    const socials = [
+        {
+            href: "#",
+            img: vk,
+            alt: "vk Petrov Konstantin"
+        },
+        {
+            href: "#",
+            img: insta,
+            alt: "pinterest Petrov Konstantin"
+        },
+        {
+            href: "#",
+            img: pinterest,
+            alt: "instagram Petrov Konstantin"
+        },
+    ]
     return <div>
-        Sidebar
+        <div className="sidebar_header">
+            <img src={sidebarHeader} alt="sidebar-header"/>
+        </div>
+        <div className="sidebar_content">
+            <div className="profile">
+                <img className="profile__avatar" src="https://place-hold.it/100" alt=""/>
+                <div className="profile__name">Константин Петров</div>
+                <div className="profile__prof">Blog: Front-end Developer</div>
+                <ul className="social">
+                    {socials.map(social => <li className="social__item">
+                        <a className="social__link" href={social.href} target="_blank">
+                            <img src={social.img} alt={social.alt}/>
+                        </a>
+                    </li>)}
+                </ul>
+                <div className="profile__text">
+                    <p>
+                        Front-end разработчик. Практик верстки сайтов.
+                        Созданием сайтов занимаюсь с 2020 года.
+                        Работал в нескольких ИТ компаниях.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className="sidebar_footer"></div>
     </div>
 }
