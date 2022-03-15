448465155db35391cb63af4ede4bd2be9d15a363
Konstantin <Steelmen87@bk.ru>
2022-03-15 20:20:53 +0300

    next 6

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 export default function Pagination() {
 
-    return <div className="pagination">
-        <h1>Pagination</h1>
+    return <div>
+        <ul className="pagination">
+            <li className="pagination__item">
+                <a className="pagination__link" href="#">&lt;</a>
+            </li>
+            <li className="pagination__item">
+                <a className="pagination__link" href="#">1</a>
+            </li>
+            <li className="pagination__item">
+                <a className="pagination__link active" href="#">2</a>
+            </li>
+            <li className="pagination__item">
+                <a className="pagination__link" href="#">3</a>
+            </li>
+            <li className="pagination__item">
+                <a className="pagination__link" href="#">&gt;</a>
+            </li>
+        </ul>
     </div>
 }
