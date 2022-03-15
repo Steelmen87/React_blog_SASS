396d28d33bb8540101ba5be52011ffe56f1c1e94
Konstantin <Steelmen87@bk.ru>
2022-03-15 12:08:53 +0300

    next 5->

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Stories from "./Stories";
 import Post from "./Post";
+import rect1 from "../assets/images/Rectangle 1.jpg"
+import rect2 from "../assets/images/Rectangle 2.jpg"
+import rect3 from "../assets/images/Rectangle 3.jpg"
+import rect4 from "../assets/images/Rectangle 4.jpg"
 
 export default function Main() {
+    const stories= [
+        {
+            src:rect1,
+            title:'Отдыхаю на природе',
+            dateTime:'2021-09-21'
+        },
+        {
+            src:rect2,
+            title:'Заканчиваю сложный проект',
+            dateTime:'2021-09-22'
+        },
+        {
+            src:rect3,
+            title:'Переехал в новую квартиру',
+            dateTime:'2021-09-23'
+        },
+        {
+            src:rect4,
+            title:'Осень заканчиваеться',
+            dateTime:'2021-09-24'
+        },
+    ]
     return <div>
-        <Stories/>
+        <Stories stories={stories}/>
         <Post/>
     </div>
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import send from "../assets/images/send.svg"
-import file from "../assets/images/photo 1.svg"
+
 export default function Post() {
+    const[content,setContent] = useState("")
+    const changeContent = (e) => {
+        setContent(e.currentTarget.value)
+    }
+
     return <div className="add-post">
         <form action="/" method="post" className="add-post__form">
             <textarea className="add-post__textarea"
-                name="post-text"
-                placeholder="Напишите что-нибудь"
-            ></textarea>
+                      value={content}
+                      onChange={changeContent}
+                      name="post-text"
+                      placeholder="Напишите что-нибудь"/>
             <div className="add-post__form-actions">
                 <label className="add-post__file" htmlFor="add-post-file">
                     <input type="file" id="add-post-file"/>
